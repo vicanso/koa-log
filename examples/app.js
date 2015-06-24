1751b315a2cf41349b3382bd648e5758fa87c20f
vicanso <vicansocanbico@gmail.com>
2015-06-24 19:59:13 +0800

    commit

---
+++ examples/app.js
@@ -0,0 +1,26 @@
+var koa = require('koa');
+var router = require('koa-router')();
+var logger = require('../lib/logger');
+
+
+
+var app = koa();
+
+var index = 0;
+router.get('/', function *(next){
+  yield function(done){
+    setTimeout(done, 3000);
+  };
+  index++;
+  this.body = 'abcd' + index;
+});
+
+
+app.use(logger('logger-test'));
+
+
+app.use(router.routes());
+
+var port = process.env.PORT || 10000;
+app.listen(port);
+console.dir('server listen on:' + port);
