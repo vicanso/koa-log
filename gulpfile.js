33d7af3850c645cf610263eef24c2ff32aa713e6
vicanso <vicansocanbico@gmail.com>
2015-12-04 21:42:15 +0800

    commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,38 @@
-var gulp = require('gulp');
-var jshint = require('gulp-jshint');
-var mocha = require('gulp-mocha');
-var istanbul = require('gulp-istanbul');
+const gulp = require('gulp');
+const jshint = require('gulp-jshint');
+const mocha = require('gulp-mocha');
+const istanbul = require('gulp-istanbul');
 
 
 gulp.task('jshint', function() {
-  return gulp.src('./lib/*.js')
-    .pipe(jshint({
-      predef : ['require', 'module'],
-      node : true,
-      esnext : true
-    }))
-    .pipe(jshint.reporter('default'));
+	return gulp.src('./lib/*.js')
+		.pipe(jshint({
+			predef: ['require', 'module'],
+			node: true,
+			esnext: true
+		}))
+		.pipe(jshint.reporter('default'));
 });
 
 
-gulp.task('default', ['jshint']);
+gulp.task('test', function(cb) {
+	gulp.src(['./lib/*.js'])
+		.pipe(istanbul())
+		.pipe(istanbul.hookRequire())
+		.on('finish', function() {
+			gulp.src(['./test/*.js'])
+				.pipe(mocha({
+					reporter: 'spec'
+				}))
+				.pipe(istanbul.writeReports({
+					reporters: ['text', 'text-summary', 'html']
+				}))
+				.pipe(istanbul.enforceThresholds({
+					thresholds: {
+						global: 80
+					}
+				})).on('end', cb);
+		});
+});
+
+gulp.task('default', ['jshint', 'test']);
